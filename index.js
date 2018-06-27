1d4f4a4948f7821232b8b85d77925a3895c091c4
nero <nero@yamato>
2018-06-27 16:14:29 +0300

    move http to https

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const select = document.getElementById('cities');
 
 const getWeather = (city) => {
     const appid = '3191198b31f62eb43fb7be2c09b18643';
-    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${appid}&units=metric`;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${appid}&units=metric`;
     fetch(url).then(res => res.json()).then(res => updateWeather(res));
 }
 
@@ -15,7 +15,7 @@ const updateWeather = (data) => {
 
     temp.innerHTML = `temperature: ${data.main.temp} °C`;
     humidity.innerHTML = `humidity: ${data.main.humidity} %`;
-    icon.src = `http://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+    icon.src = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
     wind.innerHTML = `wind: ${data.wind.speed} m/s`;
     pressure.innerHTML = `pressure: ${data.main.pressure} hpa`;
 }
